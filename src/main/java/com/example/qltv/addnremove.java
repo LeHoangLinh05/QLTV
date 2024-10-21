b3be724a02ce455da8d3f64be155a7d7c1135c36
LeHoangLinh05 <hzyugioh@gmail.com>
2024-10-21 13:54:46 +0700

    change

--- src/main/java/com/example/qltv/addnremove.java
+++ src/main/java/com/example/qltv/addnremove.java
@@ -1,7 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
