057561285488dc1dd8927142b4f742e443419f6a
LeHoangLinh05 <hzyugioh@gmail.com>
2024-11-04 21:11:29 +0700

    Create profileTest.java

---
+++ src/main/java/models/profileTest.java
@@ -0,0 +1,30 @@
+package models;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class profileTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the main FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/profile.fxml"));
+            Parent root = loader.load();
+
+            // Set up the scene with the loaded FXML file
+            Scene scene = new Scene(root, 910.4, 700); // Adjust the dimensions as needed
+            primaryStage.setTitle("Library Management System"); // Set the title of the window
+            primaryStage.setScene(scene); // Set the scene for the primary stage
+            primaryStage.show(); // Display the stage
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Launch the JavaFX application
+    }
+}
