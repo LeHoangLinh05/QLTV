a3f8f33ec20316949abcaf9ba324a3dc5faee502
vtl13905 <163302228+vtl13905@users.noreply.github.com>
2024-11-10 18:45:55 +0700

    Update Member.java

--- src/main/java/models/Member.java
+++ src/main/java/models/Member.java
@@ -1,13 +1,20 @@
 package models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Member extends User {
     private String address;
+    private List<Loan> returnHistory;
 
-    public Member() {}
+    public Member() {
+        returnHistory = new ArrayList<>();
+    }
 
     public Member(String userId, String name, String email, String phoneNumber, String address) {
         super(userId, name, email, phoneNumber);
         this.address = address;
+        this.returnHistory = new ArrayList<>();
     }
 
     public String getAddress() {
@@ -16,5 +23,13 @@ public class Member extends User {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public List<Loan> getReturnHistory() {
+        return returnHistory;
+    }
+
+    public void addReturnHistory(Loan loan) {
+        returnHistory.add(loan);
     }
 }
